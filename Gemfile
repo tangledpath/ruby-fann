1b59b968951c0c9414baa0c5521497871fda5552
Steven Miers <6759449+git-steven@users.noreply.github.com>
2018-08-23 14:12:26 -0500

    Update old dependencies and bump version.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ruby-fann.gemspec
+gem 'json'
 gem 'rdoc'
 gemspec
--- lib/ruby_fann/version.rb
+++ lib/ruby_fann/version.rb
@@ -2,7 +2,7 @@ module RubyFann
   module VERSION
     MAJOR = 1
     MINOR = 2
-    TINY  = 6
+    TINY  = 7
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
