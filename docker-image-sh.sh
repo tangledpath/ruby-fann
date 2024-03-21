889eb2a0837e5c088cf4b83e17f9aacc5b179581
Steven Miers <steven.miers@gmail.com>
2024-03-20 21:43:30 -0500

    Regen docs.

--- docker-image-sh.sh
+++ docker-image-sh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --platform linux/arm64 -it --entrypoint sh earbug:artist_harvester
+docker run --platform linux/arm64 -it --entrypoint sh tps:ruby-fann
 # docker run --platform linux/arm64 -it  earbug:artist_harvester sh
